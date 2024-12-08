40f2a06d1bcc3a4c8bffba7b088a392636591366
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-12-08 01:41:59 +0100

    Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div>
+        <h1>Bienvenue à la chasse aux œufs de Pâques !</h1>
+        <p>Explorez le jardin et trouvez les œufs cachés. Bonne chance !</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
