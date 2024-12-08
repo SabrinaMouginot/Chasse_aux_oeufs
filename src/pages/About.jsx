a76e77a12bc6d27e626138e4ae68bb27fa222c72
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-12-08 01:56:02 +0100

    About

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+    return (
+      <div>
+        <h1>À propos du jeu</h1>
+        <p>
+          Ce jeu a été développé pour célébrer Pâques avec une chasse aux œufs virtuelle.
+        </p>
+      </div>
+    );
+  }
+  
+  export default About;
+  
